6c57e5c6259596fb74cb8c9d8007f735451643cb
marqquesrafael <marqquesrafael@gmail.com>
2022-05-01 17:43:17 -0300

    inclusão de documentação com swagger.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,71 @@
 const express = require('express');
-const app = express()
+const app = express();
 const database = require('../db')
 require("dotenv").config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Avaliação GCT',
+            description: 'documentação da api de monitoramentos de placas.',
+            contact: {
+                name: 'Rafael Marques'
+            },
+            servers: ['http://localhost:5000']
+        },
+        components:{
+            schemas: require('../schemas.json')
+        }
+    },
+    apis: ['index.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 const Monitoramento = require('../monitoramento');
 
-app.get('/', async (req, res) => {
+/**
+ * @swagger
+ * /monitoramentos:
+ *  get:
+ *    tags:
+ *     - Monitoramentos
+ *    description: Retorna todos os monitoramentos cadastrados
+ *    responses:
+ *      '200':
+ *         description: Monitoramentos retornados com sucesso!
+ */
+app.get('/monitoramentos', async (req, res) => {
 
     const monitoramentos = await Monitoramento.findAll();
 
-    res.send(JSON.stringify(monitoramentos))
+    res.status(200).send(JSON.stringify(monitoramentos))
 });
 
+/**
+ * @swagger
+ * /create:
+ *   post:
+ *     tags:
+ *       - Monitoramentos
+ *     description: cadastra um novo monitoramento
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: monitoramento
+ *         description: modelo de monitoramento
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/components/schemas/Create'
+ *     responses:
+ *       200:
+ *         description: Monitoramento cadastrado com sucesso!
+ */
 app.post('/create', async (req, res) => {
 
     const { placa, data_inicio, data_termino } = req.body;
@@ -23,14 +76,33 @@ app.post('/create', async (req, res) => {
         data_termino: data_termino
     });
 
-    res.send(JSON.stringify(monitoramento));
+    res.status(200).send(JSON.stringify(monitoramento));
 });
 
-
-app.put('/update', async(req, res) => {
+/**
+ * @swagger
+ * /update:
+ *   put:
+ *     tags:
+ *       - Monitoramentos
+ *     description: atualiza um monitoramento
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: monitoramento
+ *         description: modelo de monitoramento
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/components/schemas/Monitoramento'
+ *     responses:
+ *       200:
+ *         description: monitoramento atualizado com sucesso!
+ */
+app.put('/update', async (req, res) => {
 
     const { id, placa, data_inicio, data_termino } = req.body;
-    
+
     const monitoramento = await Monitoramento.findByPk(id);
 
     monitoramento.placa = placa;
@@ -41,12 +113,31 @@ app.put('/update', async(req, res) => {
     res.send(JSON.stringify(monitoramento));
 });
 
-app.delete('/delete/:id', async(req, res) => {
-   
+/**
+ * @swagger
+ * /delete/{id}:
+ *  delete:
+ *    tags:
+ *      - Monitoramentos
+ *    description: deleta um monitoramento
+ *    responses:
+ *      '200':
+ *         description: Monitoramento deletado com sucesso!
+ *  parameters:
+ *      - name: id
+ *        description: id do monitoramento a ser excluido.
+ *        in: path
+ *        required: true
+ *        type: integer
+ *        schema:
+ *          $ref: '#/components/schemas/Monitoramento'
+ *          
+ */
+app.delete('/delete/:id', async (req, res) => {
+
     const monitoramento = await Monitoramento.findByPk(req.params.id);
 
     await monitoramento.destroy(monitoramento);
-
     res.send(JSON.stringify(monitoramento));
 });
 
